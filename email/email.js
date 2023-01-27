6b540be6c0714bb41b403d5c56d3f1a220a34c0f
rajesh <rajesht1989@gmail.com>
2023-01-27 12:02:26 +0530

    email.js implemented

---
+++ email/email.js
@@ -0,0 +1,25 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'rajeshfreshstart@gmail.com',
+      pass: ''
+    }
+    
+  });
+  
+  var mailOptions = {
+    from: 'rajeshfreshstart@gmail.com',
+    to: 'rajesht1989@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
